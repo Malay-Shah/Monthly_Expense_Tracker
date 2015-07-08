cc6797f7c3fe6a4a230389cdeb8fdbad7408e3a0
malay159 <malay159@hotmail.com>
2015-07-08 17:56:12 -0400

    Initial commit

---
+++ app/src/main/java/com/random/malay/monthlyexpensetracker/ExpenseDatabase.java
@@ -0,0 +1,14 @@
+package com.random.malay.monthlyexpensetracker;
+
+/**
+ * Created by Malay on 7/8/2015.
+ */
+public class ExpenseDatabase {
+
+    public static abstract class NewExpenseItem {
+        public static final String DATE_EXPENSE = "date";
+        public static final String DESCRIPTION = "description";
+
+    }
+
+}
