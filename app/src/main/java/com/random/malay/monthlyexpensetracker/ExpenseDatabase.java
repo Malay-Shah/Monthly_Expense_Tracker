ab0fb264ad3abd3a4741fdf0774d735da135a222
malay159 <malay159@hotmail.com>
2015-07-08 18:27:03 -0400

    Created the template of the database. Can now create a row in the database.

--- app/src/main/java/com/random/malay/monthlyexpensetracker/ExpenseDatabase.java
+++ app/src/main/java/com/random/malay/monthlyexpensetracker/ExpenseDatabase.java
@@ -6,8 +6,11 @@ package com.random.malay.monthlyexpensetracker;
 public class ExpenseDatabase {
 
     public static abstract class NewExpenseItem {
-        public static final String DATE_EXPENSE = "date";
+        public static final String DATE = "date";
         public static final String DESCRIPTION = "description";
+        public static final String CATEGORY = "category";
+        public static final String AMOUNT = "amount";
+        public static final String TABLE_NAME = "expense_info";
 
     }
 
---
+++ app/src/main/java/com/random/malay/monthlyexpensetracker/UserDbHelper.java
@@ -0,0 +1,48 @@
+package com.random.malay.monthlyexpensetracker;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by Malay on 7/8/2015.
+ */
+public class UserDbHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "EXPENSE.DB";
+    private static final int DATABASE_VERSION = 1;
+    private static final String CREATE_QUERY=
+            "CREATE TABLE " + ExpenseDatabase.NewExpenseItem.TABLE_NAME + "("+ ExpenseDatabase.NewExpenseItem.DATE+" TEXT,"+ ExpenseDatabase.NewExpenseItem.DESCRIPTION+ " TEXT,"+
+            ExpenseDatabase.NewExpenseItem.CATEGORY+" TEXT," + ExpenseDatabase.NewExpenseItem.AMOUNT+ " TEXT);";
+
+    public UserDbHelper(Context context){
+
+
+        super(context,DATABASE_NAME,null,DATABASE_VERSION);
+        Log.e("DATABASE OPERATIONS", "Database created / opened");
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL(CREATE_QUERY);
+        Log.e("DATABASE OPERATIONS", "Table created");
+
+    }
+
+    public void addInformations(String date, String description, String category, String amount,SQLiteDatabase db ){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(ExpenseDatabase.NewExpenseItem.DATE,date);
+        contentValues.put(ExpenseDatabase.NewExpenseItem.DESCRIPTION, description);
+        contentValues.put(ExpenseDatabase.NewExpenseItem.CATEGORY, category);
+        contentValues.put(ExpenseDatabase.NewExpenseItem.AMOUNT, amount);
+        db.insert(ExpenseDatabase.NewExpenseItem.TABLE_NAME, null, contentValues);
+        Log.e("DATABASE OPERATIONS", "One row inserted in DB");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
