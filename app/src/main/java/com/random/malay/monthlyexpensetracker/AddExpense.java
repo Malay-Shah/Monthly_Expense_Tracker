cd48cc26ed3b9189dd8a69a308b0b9f4b7f9315a
malay159 <malay159@hotmail.com>
2015-07-14 17:47:26 -0400

    Created the template of the database. Can now create a row in the database. Created xml layout for adding an expense. Changed the style so there is no action bar. Changed the input from TEXT to REAL in dbHelper Created a new Activity for adding an expense.(Changed it from main activity to its own seperate activity) Can now add a row in the database. Fixed some minor mistakes. Now displays the information that was added. Creates each separate row for different expense input. Added 2 new files, AddExpense and activity_add_expense.

---
+++ app/src/main/java/com/random/malay/monthlyexpensetracker/AddExpense.java
@@ -0,0 +1,42 @@
+package com.random.malay.monthlyexpensetracker;
+
+import android.app.Activity;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class AddExpense extends Activity {
+
+    EditText inputDate , inputDescription, inputCategory, inputAmount;
+    UserDbHelper userDbHelper;
+    SQLiteDatabase sqLiteDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_expense);
+        inputDate = (EditText) findViewById(R.id.inputDate);
+        inputDescription = (EditText) findViewById(R.id.inputDescription);
+        inputCategory = (EditText) findViewById(R.id.inputCategory);
+        inputAmount = (EditText) findViewById(R.id.inputAmount);
+
+    }
+
+    public void AddExpense(View view){
+        String date = inputDate.getText().toString();
+        String description = inputDescription.getText().toString();
+        String category = inputCategory.getText().toString();
+        String amount = inputAmount.getText().toString();
+        userDbHelper = new UserDbHelper(view.getContext());
+        sqLiteDatabase = userDbHelper.getWritableDatabase();
+        userDbHelper.addInformations(date, description, category, amount, sqLiteDatabase);
+        Toast.makeText(getBaseContext(),"Data Saved", Toast.LENGTH_LONG).show();
+        userDbHelper.close();
+    }
+
+}
